544f03cc41ff6c6a0339ffb1950033e858185333
Chris <chris@malouf.de>
2019-10-08 11:12:44 +0200

    implemented filteredimage endpoint

--- src/server.ts
+++ src/server.ts
@@ -13,30 +13,41 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get("/filteredimage", async (req: express.Request, res: express.Response) => {
 
-  /**************************************************************************** */
+    // validate the image_url query
+    const { image_url } = req.query;
+    if (!image_url) {
+      return res.status(400).send("missing parameter image_url");
+    }
 
-  //! END @TODO1
-  
+    // call filterImageFromURL(image_url) to filter the image
+    let image_payload = await filterImageFromURL(image_url);
+    if (!image_payload) {
+      return res.status(404).send("unable to filter image");
+    }
+
+    // send the resulting file in the response
+    res.status(200).contentType("image/jpeg").sendFile(image_payload);
+
+    // deletes any files on the server on finish of the response
+    res.on("finish", (_: any) => {
+      console.log(`on finish response event - delete image ${image_payload}`);
+      deleteLocalFiles([`${image_payload}`]);
+    });
+  });
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
 
   // Start the Server
   app.listen( port, () => {
